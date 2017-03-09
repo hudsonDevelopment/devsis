427ae9864074918d0fa524255228eee32f8e7efb
Hudson Andrade <hudson.andrade@gmail.com>
2017-03-09 16:44:11 -0300

    aulas 01 e 02

---
+++ 02/condicional/condicional/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace condicional
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titulo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.boxmeses = new System.Windows.Forms.ComboBox();
+            this.retmes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.titulo.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.Location = new System.Drawing.Point(110, 19);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(146, 22);
+            this.titulo.TabIndex = 0;
+            this.titulo.Text = "aula de condicional";
+            this.titulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.titulo.Click += new System.EventHandler(this.titulo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Desenvolvimento de Software";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(181, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Informe sua idade:";
+            this.label1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // boxmeses
+            // 
+            this.boxmeses.FormattingEnabled = true;
+            this.boxmeses.Items.AddRange(new object[] {
+            "janeiro",
+            "fevereiro",
+            "marco",
+            "abril",
+            "maio",
+            "junho",
+            "julho",
+            "agosto",
+            "setembro",
+            "outubro",
+            "novembro",
+            "dezembro"});
+            this.boxmeses.Location = new System.Drawing.Point(129, 104);
+            this.boxmeses.Name = "boxmeses";
+            this.boxmeses.Size = new System.Drawing.Size(127, 21);
+            this.boxmeses.TabIndex = 7;
+            this.boxmeses.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // retmes
+            // 
+            this.retmes.AutoSize = true;
+            this.retmes.Location = new System.Drawing.Point(126, 211);
+            this.retmes.Name = "retmes";
+            this.retmes.Size = new System.Drawing.Size(35, 13);
+            this.retmes.TabIndex = 8;
+            this.retmes.Text = "";
+            this.retmes.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 281);
+            this.Controls.Add(this.retmes);
+            this.Controls.Add(this.boxmeses);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.titulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox boxmeses;
+        private System.Windows.Forms.Label retmes;
+    }
+}
+
